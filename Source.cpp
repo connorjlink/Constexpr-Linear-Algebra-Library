8857e56ec6792fcec2d34af92564d6ca8a48fa4a
Shmizmin <connorlink248@gmail.com>
2020-10-28 16:54:46 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,469 @@
+#include <iostream>
+#include <tuple>
+#include <utility>
+#include <functional>
+#include <algorithm>
+#include <type_traits>
+#include <array>
+#include <cmath>
+
+#ifndef __cpp_lib_concepts
+#define __cpp_lib_concepts
+#include <concepts>
+#endif
+
+#include "gcem.hpp"
+
+namespace std
+{
+	using int_t = int;
+	using float_t = float;
+	using double_t = double;
+	using long_double_t = long double;
+	using signed_t = signed;
+	using unsigned_t = unsigned;
+	using long_t = long;
+	using long_long_t = long long;
+	using short_t = short;
+
+	constexpr auto pi_t = 3.14159265f;
+	constexpr auto half_pi_t = pi_t / 2.0f;
+	constexpr auto two_pi_t = pi_t * 2.0f;
+
+	template<typename V, typename... Ts>
+	constexpr auto make_array(Ts&&... t) noexcept -> std::array<V, sizeof...(Ts)>
+	{
+		return { { std::forward<Ts>(t)... } };
+	}
+
+	template<class... Formats, std::size_t N, std::size_t... Is, typename T>
+	constexpr std::tuple<Formats...> as_tuple(const std::array<T, N>& arr, std::index_sequence<Is...>) noexcept
+	{
+		return std::make_tuple(Formats{ arr[Is] }...);
+	}
+
+	template<class... Formats, typename T, std::size_t N>
+	constexpr std::tuple<Formats...> as_tuple(const std::array<T, N>& arr) noexcept requires (N == sizeof...(Formats))
+	{
+		return as_tuple<Formats...>(arr, std::make_index_sequence<N>{});
+	}
+
+	template<typename tuple_t>
+	constexpr auto as_array(tuple_t&& tuple) noexcept
+	{
+		constexpr auto get_array = [](auto&& ... x) { return std::array{ std::forward<decltype(x)>(x) ... }; };
+		return std::apply(get_array, std::forward<tuple_t>(tuple));
+	}
+}
+
+namespace core
+{
+	//define the matrix alias to a c++ array
+	template<std::size_t N, typename T>
+	using matrix = std::array<std::array<T, N>, N>;
+
+	//define matrix type aliases
+	using int2x2 = matrix<2, std::int_t>;
+	using int3x3 = matrix<3, std::int_t>;
+	using int4x4 = matrix<4, std::int_t>;
+
+	using float2x2 = matrix<2, std::float_t>;
+	using float3x3 = matrix<3, std::float_t>;
+	using float4x4 = matrix<4, std::float_t>;
+
+	using double2x2 = matrix<2, std::double_t>;
+	using double3x3 = matrix<3, std::double_t>;
+	using double4x4 = matrix<4, std::double_t>;
+
+	//define vector type aliases
+	using int2 = std::tuple<std::int_t, std::int_t>;
+	using int3 = std::tuple<std::int_t, std::int_t, std::int_t>;
+	using int4 = std::tuple<std::int_t, std::int_t, std::int_t, std::int_t>;
+
+	using float2 = std::tuple<std::float_t, std::float_t>;
+	using float3 = std::tuple<std::float_t, std::float_t, std::float_t>;
+	using float4 = std::tuple<std::float_t, std::float_t, std::float_t, std::float_t>;
+
+	using double2 = std::tuple<std::double_t, std::double_t>;
+	using double3 = std::tuple<std::double_t, std::double_t, std::double_t>;
+	using double4 = std::tuple<std::double_t, std::double_t, std::double_t, std::double_t>;
+
+	namespace trig
+	{
+		template<typename T = std::float_t>
+		constexpr auto radians(T&& degrees) noexcept requires std::floating_point<T>
+		{
+			return (std::move(degrees) / (T)57.295779513082320876798154814105);
+		}
+
+		template<typename T = std::float_t>
+		constexpr auto radians(const T& degrees) noexcept requires std::floating_point<T>
+		{
+			return (degrees / (T)57.295779513082320876798154814105);
+		}
+
+		template<typename T = std::float_t>
+		constexpr auto degrees(T&& radians) noexcept requires std::floating_point<T>
+		{
+			return (std::move(radians) * (T)57.295779513082320876798154814105);
+		}
+
+		template<typename T = std::float_t>
+		constexpr auto degrees(const T& radians) noexcept requires std::floating_point<T>
+		{
+			return (radians * (T)57.295779513082320876798154814105);
+		}
+	}
+
+	namespace vec
+	{
+		template<typename binaryop = std::multiplies<void>, typename T, typename... Ts>
+		constexpr auto apply(const std::tuple<Ts...>& vec, T&& operand) noexcept
+		{
+			std::tuple<Ts...> outVec;
+
+			[&]<std::size_t... i>(std::index_sequence<i...>)
+			{
+				((std::get<i>(outVec) = binaryop{}(std::get<i>(vec), operand)), ...);
+			}
+			(std::make_index_sequence<sizeof...(Ts)>{});
+
+			return outVec;
+		}
+
+		template<typename binaryop = std::multiplies<void>, typename... Ts>
+		constexpr auto apply(const std::tuple<Ts...>& vec1, const std::tuple<Ts...>& vec2) noexcept
+		{
+			std::tuple<Ts...> outVec;
+
+			[&] <std::size_t... i>(std::index_sequence<i...>)
+			{
+				((std::get<i>(outVec) = binaryop{}(std::get<i>(vec1), std::get<i>(vec2))), ...);
+			}
+			(std::make_index_sequence<sizeof...(Ts)>{});
+
+			return outVec;
+		}
+
+		template<typename... Ts>
+		constexpr auto sum(const std::tuple<Ts...>& vec) noexcept
+		{
+			return std::apply([](auto&&... v) { return (v + ...); }, vec);
+		}
+
+		template<typename... Ts>
+		constexpr auto lengthSquared(const std::tuple<Ts...>& vec) noexcept
+		{
+			return sum(vec::apply<std::multiplies<>>(vec, vec));
+		}
+
+		template<typename... Ts>
+		constexpr auto length(const std::tuple<Ts...>& vec) noexcept
+		{
+			return gcem::sqrt(lengthSquared(vec));
+		}
+
+		template<typename... Ts>
+		constexpr auto normalize(const std::tuple<Ts...>& vec) noexcept
+		{
+			return vec::apply<std::divides<>>(vec, (length(vec)));
+		}
+
+		template<bool Normalize = false, typename... Ts>
+		constexpr auto dot(const std::tuple<Ts...>& vec1, const std::tuple<Ts...>& vec2) noexcept
+		{
+			if constexpr (Normalize) return sum(std::tuple<Ts...>{ vec::apply<std::multiplies<>>(normalize(vec1), normalize(vec2)) });
+			else return sum(std::tuple<Ts...>{ vec::apply<std::multiplies<>>(vec1, vec2) });
+		}
+
+		template<typename... Ts>
+		constexpr auto cross(const std::tuple<Ts...>& vec1, const std::tuple<Ts...>& vec2) noexcept requires (sizeof...(Ts) == 3)
+		{
+			return std::make_tuple
+			(
+				((std::get<1>(vec1) * std::get<2>(vec2)) - (std::get<2>(vec1) * std::get<1>(vec2))),
+				((std::get<2>(vec1) * std::get<0>(vec2)) - (std::get<0>(vec1) * std::get<2>(vec2))),
+				((std::get<0>(vec1) * std::get<1>(vec2)) - (std::get<1>(vec1) * std::get<0>(vec2)))
+			);
+		}
+
+		template<typename T, std::size_t S, typename... Ts>
+		constexpr auto mul(const std::tuple<Ts...>& vec, const matrix<S, T>& mat) noexcept requires (S == sizeof...(Ts))
+		{
+			std::array<T, S> outVec;
+
+			std::array<T, S> inVec = std::as_array(vec);
+
+			for (auto i = 0; i < S; ++i)
+			{
+				outVec[i] = 0;
+
+				for (auto j = 0; j < S; ++j)
+				{
+					outVec[i] += mat[i][j] * inVec[j];
+				}
+			}
+
+			return std::as_tuple<Ts...>(outVec);
+		}
+	}
+
+	namespace mat
+	{
+		template<typename binaryop = std::multiplies<void>, typename T, std::size_t S>
+		constexpr auto apply(const matrix<S, T>& mat, T&& operand)
+		{
+			matrix<S, T> outMat;
+
+			for (auto i = 0; i < S; ++i)
+			{
+				for (auto j = 0; j < S; ++j)
+				{
+					outMat[i][j] = binaryop{}(mat[i][j], operand);
+				}
+			}
+
+			return outMat;
+		}
+
+		template<typename binaryop = std::multiplies<void>, typename T, std::size_t S>
+		constexpr auto apply(const matrix<S, T>& mat1, const matrix<S, T>& mat2)
+		{
+			matrix<S, T> outMat;
+
+			for (auto i = 0; i < S; ++i)
+			{
+				for (auto j = 0; j < S; ++j)
+				{
+					outMat[i][j] = binaryop{}(mat1[i][j], mat2[i][j]);
+				}
+			}
+
+			return outMat;
+		}
+
+		template<typename T, std::size_t S>
+		constexpr auto compose(const matrix<S, T>& m1, const matrix<S, T>& m2) noexcept
+		{
+			matrix<S, T> m3 = {};
+
+			for (auto i = 0; i < S; i++)
+			{
+				for (auto j = 0; j < S; j++)
+				{
+					auto sum = 0;
+
+					for (auto k = 0; k < S; k++)
+					{
+						sum += m1[i][k] * m2[k][j];
+					}
+
+					m3[i][j] = sum;
+				}
+			}
+
+			return m3;
+		}
+
+		template<typename T>
+		constexpr auto identity() noexcept
+		{
+			matrix<4, T> mat;
+
+			mat[0][0] = 1.0f;
+			mat[1][1] = 1.0f;
+			mat[2][2] = 1.0f;
+			mat[3][3] = 1.0f;
+
+			return mat;
+		}
+
+		template<typename T>
+		constexpr auto rotationX(T&& angle) noexcept requires std::floating_point<T>
+		{
+			matrix<4, T> mat;
+
+			mat[0][0] = 1.0f;
+			mat[1][1] = gcem::cos(angle);
+			mat[1][2] = gcem::sin(angle);
+			mat[2][1] = -gcem::sin(angle);
+			mat[2][2] = gcem::cos(angle);
+			mat[3][3] = 1.0f;
+
+			return mat;
+		}
+
+		template<typename T>
+		constexpr auto rotationY(T&& angle) noexcept requires std::floating_point<T>
+		{
+			matrix<4, T> mat;
+
+			mat[0][0] = gcem::cos(angle);
+			mat[0][2] = gcem::sin(angle);
+			mat[2][0] = -gcem::sin(angle);
+			mat[1][1] = 1.0f;
+			mat[2][2] = gcem::cos(angle);
+			mat[3][3] = 1.0f;
+
+			return mat;
+		}
+
+		template<typename T>
+		constexpr auto rotationZ(T&& angle) noexcept requires std::floating_point<T>
+		{
+			matrix<4, T> mat;
+
+			mat[0][0] = gcem::cos(angle);
+			mat[0][1] = gcem::sin(angle);
+			mat[1][0] = -gcem::sin(angle);
+			mat[1][1] = gcem::cos(angle);
+			mat[2][2] = 1.0f;
+			mat[3][3] = 1.0f;
+
+			return mat;
+		}
+
+		template<typename T>
+		constexpr auto rotationXYZ(T&& x, T&& y, T&& z) noexcept requires std::floating_point<T>
+		{
+			auto rotX = rotationX(x);
+			auto rotY = rotationY(y);
+			auto rotZ = rotationZ(z);
+
+			auto rotXY = compose(rotX, rotY);
+			auto rotXYZ = compose(rotXY, rotZ);
+
+			return rotXYZ;
+		}
+
+		template<typename T>
+		constexpr auto translation(T&& x, T&& y, T&& z) noexcept
+		{
+			matrix<4, T> mat;
+
+			mat[0][0] = 1.0f;
+			mat[1][1] = 1.0f;
+			mat[2][2] = 1.0f;
+			mat[3][3] = 1.0f;
+			mat[3][0] = x;
+			mat[3][1] = y;
+			mat[3][2] = z;
+
+			return mat;
+		}
+
+		template<typename T>
+		constexpr auto projection(T&& fov, T&& aspectRatio, T&& near, T&& far) noexcept requires std::floating_point<T>
+		{
+			T fovRad = (T)1 / gcem::tan(fov * (T)0.5 / (T)180 * (T)3.14159265359);
+
+			matrix<4, T> mat;
+
+			mat[0][0] = aspectRatio * fovRad;
+			mat[1][1] = fovRad;
+			mat[2][2] = far / (far - near);
+			mat[3][2] = (-far * near) / (far - near);
+			mat[2][3] = 1.0f;
+			mat[3][3] = 0.0f;
+
+			return mat;
+		}
+
+		template<typename T, typename... Ts>
+		constexpr auto pointAt(const std::tuple<Ts...>& pos, const std::tuple<Ts...>& target, const std::tuple<Ts...>& up) noexcept requires (sizeof...(Ts) == 3) or (sizeof...(Ts) == 4)
+		{
+			auto newForward = apply<std::minus<>>(target, pos);
+			auto newForwardNormalized = normalize(newForward);
+
+			auto a = apply<std::multiplies<>>(newForwardNormalized, dot(up, newForwardNormalized));
+			auto newUp = apply<std::minus<>>(up, a);
+			auto newUpNormalized = normalize(newUp);
+
+			auto newRight = cross(newUpNormalized, newForwardNormalized);
+
+			matrix<4, T> mat;
+
+			mat[0][0] = std::get<0>(newRight);
+			mat[0][1] = std::get<1>(newRight);
+			mat[0][2] = std::get<2>(newRight);
+			mat[0][3] = 0.0f;
+
+			mat[1][0] = std::get<0>(newUpNormalized);
+			mat[1][1] = std::get<1>(newUpNormalized);
+			mat[1][2] = std::get<2>(newUpNormalized);
+			mat[1][3] = 0.0f;
+
+			mat[2][0] = std::get<0>(newForwardNormalized);
+			mat[2][1] = std::get<1>(newForwardNormalized);
+			mat[2][2] = std::get<2>(newForwardNormalized);
+			mat[2][3] = 0.0f;
+
+			mat[3][0] = std::get<0>(pos);
+			mat[3][1] = std::get<1>(pos);
+			mat[3][2] = std::get<2>(pos);
+			mat[3][3] = 1.0f;
+
+			return mat;
+
+			return matrix<4, T>
+			{
+				std::get<0>(newRight), std::get<1>(newRight), std::get<2>(newRight), 0.0f,
+				std::get<0.(newUpNormalized), std::get<1>(newUpNormalized), std::get<2>(newUpNormalized), 0.0f,
+
+			};
+		}
+	}
+}
+
+template<typename T, std::size_t S>
+std::ostream& operator<<(std::ostream& stream, const core::matrix<S, T>& mat) noexcept
+{
+	stream << "[ ";
+
+	for (auto i = 0; i < S; ++i)
+	{
+		for (auto j = 0; j < S; ++j)
+		{
+			stream << mat[i][j];
+
+			if (j != (S - 1)) stream << ' ';
+		}
+
+		stream << " ]" << '\n';
+
+		if (i != (S - 1)) stream << "[ ";
+	}
+
+	return stream;
+}
+
+template<typename... Ts>
+std::ostream& operator<<(std::ostream& stream, const std::tuple<Ts...>& vec) noexcept
+{
+	stream << "[ ";
+	
+	[&] <std::size_t... i>(std::index_sequence<i...>)
+	{
+		(( stream << std::get<i>(vec) << ", " ) , ...);
+	} (std::make_index_sequence<(sizeof...(Ts) - 1)>{});
+
+
+	stream << std::get<(sizeof...(Ts) - 1)>(vec) << " ]";
+
+	return stream;
+}
+
+
+int main()
+{
+	using namespace core;
+
+	constexpr int4x4 a = { 2, 3, 1, 4, 1, 4, 5, 8, 5, 1, 4, 5, 4, 2, 7, 5 };
+	constexpr int4 b = { 2, 5, 7, 3 };
+
+	constexpr int4 c = vec::mul(b, a);
+
+	std::cout << c << std::endl;
+
+	return 0;
+}
---
+++ gcem.hpp
@@ -0,0 +1,81 @@
+#ifndef _gcem_HPP
+#define _gcem_HPP
+
+#include "gcem_options.hpp"
+
+namespace gcem
+{
+    #include "gauss_legendre_50.hpp"
+
+    #include "is_inf.hpp"
+    #include "is_nan.hpp"
+    #include "is_finite.hpp"
+
+    #include "abs.hpp"
+    #include "ceil.hpp"
+    #include "floor.hpp"
+    #include "trunc.hpp"
+    #include "is_odd.hpp"
+    #include "is_even.hpp"
+    #include "max.hpp"
+    #include "min.hpp"
+    #include "sqrt.hpp"
+
+    #include "signbit.hpp"
+    #include "copysign.hpp"
+    #include "neg_zero.hpp"
+    #include "sgn.hpp"
+
+    #include "find_exponent.hpp"
+    #include "find_fraction.hpp"
+    #include "find_whole.hpp"
+    #include "mantissa.hpp"
+    #include "round.hpp"
+    #include "fmod.hpp"
+
+    #include "pow_integral.hpp"
+    #include "exp.hpp"
+    #include "expm1.hpp"
+    #include "log.hpp"
+    #include "log1p.hpp"
+    #include "log2.hpp"
+    #include "pow.hpp"
+
+    #include "gcd.hpp"
+    #include "lcm.hpp"
+
+    #include "tan.hpp"
+    #include "cos.hpp"
+    #include "sin.hpp"
+
+    #include "atan.hpp"
+    #include "atan2.hpp"
+    #include "acos.hpp"
+    #include "asin.hpp"
+
+    #include "tanh.hpp"
+    #include "cosh.hpp"
+    #include "sinh.hpp"
+
+    #include "atanh.hpp"
+    #include "acosh.hpp"
+    #include "asinh.hpp"
+
+    #include "binomial_coef.hpp"
+    #include "lgamma.hpp"
+    #include "tgamma.hpp"
+    #include "factorial.hpp"
+    #include "lbeta.hpp"
+    #include "beta.hpp"
+    #include "lmgamma.hpp"
+    #include "log_binomial_coef.hpp"
+
+    #include "erf.hpp"
+    #include "erf_inv.hpp"
+    #include "incomplete_beta.hpp"
+    #include "incomplete_beta_inv.hpp"
+    #include "incomplete_gamma.hpp"
+    #include "incomplete_gamma_inv.hpp"
+}
+
+#endif
